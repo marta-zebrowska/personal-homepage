c27b4da0bd925c41001f63b7984b0e8f2f075419
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-29 13:17:40 +0200

    color, letter spacing, word break added to GS

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -11,9 +11,10 @@ html {
 
 body {
   font-family: "Inter", sans-serif;
-  background-color: ${({theme}) => theme.color.mercury}
-}
-
+  background-color: ${({ theme }) => theme.color.mercury};
+  color: ${({ theme }) => theme.color.slategray};
+        letter-spacing: 0.05em;
+        word-break: break-word;}
 button {
     cursor: pointer;
 }
