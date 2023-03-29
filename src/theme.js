c040c8466b88c460e9085250b52d67566166c225
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-29 10:49:28 +0200

    colors added to theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,11 @@
 export const theme = {
-    color: {
-        mercury: "#E5E5E5",
-    },
+  color: {
+    white: "#FFFFFF",
+    ironTransparent: "rgba(209, 213, 218, 0.3)",
+    mercury: "#E5E5E5",
+    slategray: "#6E7E91",
+    scienceblue: "#0366D6",
+    violet: "#090A33",
+    mineshaft: "#252525",
+  },
 };
