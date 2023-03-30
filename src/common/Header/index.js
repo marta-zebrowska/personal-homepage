aa42981affe55c15d8d2f1fd26c452af1e02e760
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-30 11:36:30 +0200

    Header component created

---
+++ src/common/Header/index.js
@@ -0,0 +1,27 @@
+import {
+  About,
+  Content,
+  HireMe,
+  HireMeIcon,
+  Image,
+  Name,
+  ThisIs,
+  Wrapper,
+} from "./styled";
+import myPhoto from "./images/marta.png";
+import hireMeIcon from "./images/message.svg";
+
+export const Header = () => (
+  <Wrapper>
+    <Image src={myPhoto} alt="Marta"/>
+    <About>
+      <ThisIs>This is</ThisIs>
+      <Name>Marta Żebrowska-Giers</Name>
+      <Content>I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities.</Content>
+      <HireMe>
+        <HireMeIcon src={hireMeIcon} alt="" />
+        Hire Me
+      </HireMe>
+    </About>
+  </Wrapper>
+);
