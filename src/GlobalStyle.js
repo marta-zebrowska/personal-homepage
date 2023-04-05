44a8638dfb801dd2bd37377e2f78282e3030a8cd
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-04-05 10:07:41 +0200

    background color changed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -11,7 +11,7 @@ html {
 
 body {
   font-family: "Inter", sans-serif;
-  background-color: ${({ theme }) => theme.color.mercury};
+  background-color: ${({ theme }) => theme.color.whiteLilac};
   color: ${({ theme }) => theme.color.slategray};
         letter-spacing: 0.05em;
         word-break: break-word;}
