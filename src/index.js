9593998b9b929fea00d2b91fd260be0e98c17cd9
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-06-07 14:04:37 +0200

    index changed

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/App";
 import reportWebVitals from "./reportWebVitals";
-import { GlobalStyle } from "./core/App/GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import store from "./core/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+    <Provider store={store}>
       <App />
-    </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
@@ -20,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
