8ac2cb437c7a570c7c2c6728bcb3e7aa556b29f8
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-06-07 14:41:19 +0200

    PersonalHomepage component relocated

---
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -0,0 +1,21 @@
+import { Container } from "./Container/styled";
+import { Header } from "./Header";
+import { Portfolio } from "./Portfolio";
+import { Skills } from "./Skills";
+import { skills, toLearn } from "./Skills/skillsList";
+
+export const PersonalHomepage = () => (
+    <Container>
+      <Header />
+      <Skills
+      title="My skillset includes 🛠️"
+      skills={skills}
+      />
+      <Skills
+      title="What I want to learn next 🚀"
+      skills={toLearn} 
+      />
+      <Portfolio />
+    </Container>
+   );
+   
