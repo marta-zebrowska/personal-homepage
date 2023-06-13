821e29f08b824e80fb799eb08762d1cf4ad1a272
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-06-13 14:59:39 +0200

    personalHomepageAPI fixed

--- src/features/personalHomepage/personalHomepageAPI.js
+++ src/features/personalHomepage/personalHomepageAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const githubAPIBaseURL = "https://api.github.com";
+export const githubAPIBaseURL = "https://api.github.com";
 
 export const getRepositories = username =>
-axios.get('${githubAPIBaseURL}/users/${username}/repos')
-.then(response => response.data);
+  axios.get(`${githubAPIBaseURL}/users/${username}/repos`)
+    .then(response => response.data);
