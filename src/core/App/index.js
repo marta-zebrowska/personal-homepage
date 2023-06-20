716bc3e1f4eeea720c38e64aba6c43a494f15a18
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-06-20 12:30:08 +0200

    ThemeProvider changed

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
-
-import { theme } from "../../theme";
+import { darkTheme, lightTheme } from "../../theme";
 import { PersonalHomepage } from "../../features/personalHomepage/PersonalHomepage";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../common/themeSlice";
 
-export const App = () => {
+function App() {
+  const isDarkTheme = useSelector(selectIsDarkTheme)
+  
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <Normalize />
       <GlobalStyle />
       <PersonalHomepage />
