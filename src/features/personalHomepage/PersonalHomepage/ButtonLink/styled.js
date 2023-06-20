a62e53c4666c92ecf9e240737e8fb51b1171ad5b
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-06-20 12:31:12 +0200

    ButtonLink's styles changed

--- src/features/personalHomepage/PersonalHomepage/ButtonLink/styled.js
+++ src/features/personalHomepage/PersonalHomepage/ButtonLink/styled.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 export const ButtonLink = styled.a`
-    text-decoration: none;
-    border: 1px solid ${({ theme }) => theme.color.scienceblue};
-    color: ${({ theme }) => theme.color.scienceblue};
-    background: ${({ theme }) => theme.color.mercury};
-    font-size: 20px;
-    font-weight: 600;
-    padding: 12px 16px;
-    transition: box-shadow 0.3s;
+  text-decoration: none;
+  border: 1px solid ${({ theme }) => theme.color.buttonLink.border};
+  color: ${({ theme }) => theme.color.buttonLink.text};
+  background: ${({ theme }) => theme.color.primary};
+  font-size: 20px;
+  font-weight: 600;
+  padding: 12px 16px;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: box-shadow 0.3s;
+  &:hover {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.color.buttonLink.shadow};
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 18px;
+  }
 `;
