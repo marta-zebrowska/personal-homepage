af902df6cc29cb00928f138f51c2c1859cc0515a
Marta Żebrowska-Giers <115993282+marta-zebrowska@users.noreply.github.com>
2023-06-20 12:26:51 +0200

    themeSlice created

---
+++ src/common/themeSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+    name: "theme",
+    initialState: {
+        isDarkTheme: false,
+    },
+    reducers: {
+        toggleTheme: state => {
+            state.isDarkTheme = !state.isDarkTheme;
+        },
+    },
+});
+
+export const {
+    toggleTheme,
+} = themeSlice.actions;
+
+export const selectIsDarkTheme = state => state.theme.isDarkTheme;
+
+export default themeSlice.reducer;
