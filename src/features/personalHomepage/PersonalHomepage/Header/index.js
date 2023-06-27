6c93a041b720308695d187918d7a3ecf283c5cc8
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-06-27 12:31:34 +0200

    email added to HireMe

--- src/features/personalHomepage/PersonalHomepage/Header/index.js
+++ src/features/personalHomepage/PersonalHomepage/Header/index.js
@@ -9,17 +9,20 @@ import {
   Wrapper,
 } from "./styled";
 import myPhoto from "./images/marta.png";
-import hireMeIcon from "./images/message.svg";
+import { email } from "../email";
 
 export const Header = () => (
   <Wrapper>
-    <Image src={myPhoto} alt="Marta Żebrowska-Giers"/>
+    <Image src={myPhoto} alt="Marta Żebrowska-Giers" />
     <About>
       <ThisIs>This is</ThisIs>
       <Name>Marta Żebrowska-Giers</Name>
-      <Content>I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities.</Content>
-      <HireMe>
-        <HireMeIcon src={hireMeIcon} alt="" />
+      <Content>
+        I’m a passionate Frontend Developer in love with React, currently
+        looking for new job opportunities.
+      </Content>
+      <HireMe href={`mailto:${email}`} title={email} rel="noopener noreferrer">
+        <HireMeIcon />
         Hire Me
       </HireMe>
     </About>
