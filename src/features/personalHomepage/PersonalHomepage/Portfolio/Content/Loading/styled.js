bb4ea993a53be69be77bb2fd87a98aa3ce1e848f
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-06-30 11:49:03 +0200

    styles added

--- src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Loading/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Loading/styled.js
@@ -8,17 +8,27 @@ const rotate = keyframes`
 `;
 
 export const Wrapper = styled.div`
-    text-align: center;
-    margin-top: 88px;
-    font-size: 20px;
-    color: ${({ theme }) => theme.color.scienceblue};
-    display: grid;
-    justify-items: center;
+  text-align: center;
+  margin-top: 88px;
+  font-size: 20px;
+  color: ${({ theme }) => theme.color.textPrimary};
+  display: grid;
+  justify-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 17px;
+    margin-top: 32px;
+  }
 `;
 
 export const StyledSpinner = styled(Spinner)`
-    margin-top: 48px;
-    animation: ${rotate} 1s linear infinite;
-    color: ${({ theme }) => theme.color.scienceblue};
-    height: auto;
+  margin-top: 48px;
+  animation: ${rotate} 1s linear infinite;
+  color: ${({ theme }) => theme.color.primary};
+  height: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 60px;
+    margin-top: 32px;
+  }
 `;
