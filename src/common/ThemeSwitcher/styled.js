8f337e53e12b829dbef09543814a4080070c847b
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-06-30 11:51:50 +0200

    code formatted

--- src/common/ThemeSwitcher/styled.js
+++ src/common/ThemeSwitcher/styled.js
@@ -3,53 +3,55 @@ import { css } from "styled-components";
 import { ReactComponent as SunIcon } from "./sun.svg";
 
 export const ThemeSwitcherWrapper = styled.div`
-   margin: 60px auto 0;
-   display: flex;
-   justify-content: flex-end;
+  margin: 60px auto 0;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 export const Button = styled.button`
-    background: none;
-    border: none;
-    display: flex;
-    align-items: center;
-    color: inherit;
-    outline-offset: 8px;
-     cursor: pointer;
+  background: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  color: inherit;
+  outline-offset: 8px;
+  cursor: pointer;
 `;
 
 export const Text = styled.span`
-    font-size: 12px;
-    text-transform: uppercase;
-    font-weight: bold;
-    margin-right: 12px;
+  font-size: 12px;
+  text-transform: uppercase;
+  font-weight: bold;
+  margin-right: 12px;
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        display: none;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    display: none;
+  }
 `;
 
 export const Switcher = styled.span`
-    background: ${({ theme }) => theme.color.themeSwitcher.background};
-    border: solid 1px;
-    padding: 3px;
-    border-radius: 24px;
-    width: 48px;
-    display: flex;
+  background: ${({ theme }) => theme.color.themeSwitcher.background};
+  border: solid 1px;
+  padding: 3px;
+  border-radius: 24px;
+  width: 48px;
+  display: flex;
 `;
 
 export const IconWrapper = styled.span`
-    background: currentColor;
-    padding: 3px;
-    border-radius: 50%;
-    display: flex;
-    transition: transform 0.3s;
+  background: currentColor;
+  padding: 3px;
+  border-radius: 50%;
+  display: flex;
+  transition: transform 0.3s;
 
-    ${({ moveToRight }) => moveToRight && css`
-        transform: translateX(20px);
+  ${({ moveToRight }) =>
+    moveToRight &&
+    css`
+      transform: translateX(20px);
     `}
 `;
 
 export const Icon = styled(SunIcon)`
-    color: ${({ theme }) => theme.color.themeSwitcher.icon};
+  color: ${({ theme }) => theme.color.themeSwitcher.icon};
 `;
