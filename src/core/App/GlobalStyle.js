1501a8bbeed168ff9bf75560396661b0ea75304a
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-06-30 11:52:34 +0200

    code formatted

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -12,19 +12,19 @@ html {
 body {
   font-family: "Inter", sans-serif;
   background-color: ${({ theme }) => theme.color.site.background};
-        color: ${({ theme }) => theme.color.site.text};
-        letter-spacing: 0.05em;
-        word-break: break-word;
-        padding-bottom: 108px;
-        transition: background 0.3s;
+  color: ${({ theme }) => theme.color.site.text};
+  letter-spacing: 0.05em;
+  word-break: break-word;
+  padding-bottom: 108px;
+  transition: background 0.3s;
 
-        @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-            padding-bottom: 32px;
-        }
-      }
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    padding-bottom: 32px;
+    }
+  }
     
 
 button {
-    cursor: pointer;
+  cursor: pointer;
 }
 `;
